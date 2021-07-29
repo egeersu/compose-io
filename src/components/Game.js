3b3eae86097860a7fc3f0d94ebd310f095df2833
Ege <egeersu@gmail.com>
2021-07-29 12:18:18 +0100

    balance3

--- src/components/Game.js
+++ src/components/Game.js
@@ -45,7 +45,7 @@ const Game = (props) => {
                     move()
                     check_reachable(playerX, playerY)
                     updateZombieDistance(playerX, playerY, takeDamage)
-                    starve(0.03)    
+                    starve(0.06)    
                 }
                 clockTick()    
                 
