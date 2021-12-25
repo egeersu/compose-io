b73914244cc3df9e676682fa433609c2e9a2c3de
Ege <egeersu@gmail.com>
2021-12-26 02:24:08 +0300

    prolific id

--- src/components/Crafting/CraftingScreen.js
+++ src/components/Crafting/CraftingScreen.js
@@ -24,7 +24,7 @@ const CraftingScreen = (props) => {
         opacity: display_message ? '20%' : '100%'
     }
 
-    const [inputList, outputList, success, addItem, removeItem, collectItem, craft, itemHovered, setitemHovered] = useCraft(props.inventory, props.experimentId, props.sessionId, props.group, props.day, props.wso, props.saveData, props.prolificID)
+    const [inputList, outputList, success, addItem, removeItem, collectItem, craft, itemHovered, setitemHovered] = useCraft(props.inventory, props.experimentId, props.sessionId, props.prolificID, props.group, props.day, props.wso, props.saveData)
 
     return (
         <>
