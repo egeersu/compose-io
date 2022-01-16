d16d8831e17c06b2d5c9d043e2ef8c816b7f07c0
Ege <egeersu@gmail.com>
2022-01-16 17:37:01 +0300

    groups 1 and 2

--- src/App.js
+++ src/App.js
@@ -87,8 +87,7 @@ function App() {
     <>
       {blacklist ? 
         <Blacklist setblacklist={setblacklist}/> :
-        // <Game experimentId={experimentId} sessionId={sessionId} group={group} airtable_base={airtable_base} wso={wso} ip={ip} saveData={saveData} prolificID={prolificID} setprolificID={setprolificID}/> 
-           <Game experimentId={experimentId} sessionId={sessionId} group={4} airtable_base={airtable_base} wso={wso} ip={ip} saveData={saveData} prolificID={prolificID} setprolificID={setprolificID}/> 
+        <Game experimentId={experimentId} sessionId={sessionId} group={group} airtable_base={airtable_base} wso={wso} ip={ip} saveData={saveData} prolificID={prolificID} setprolificID={setprolificID}/> 
      }
     </>
   );
